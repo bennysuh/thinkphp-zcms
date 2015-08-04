2467c0d1c8a48ab1896c1b1c04e02f655527e0fa
qhorse <qhorse2015@163.com>
2015-08-04 17:33:18 +0800

    first commit

---
+++ Application/Home/Controller/IndexController.class.php
@@ -0,0 +1,22 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | Author: Jroy 
+// +----------------------------------------------------------------------
+
+namespace Home\Controller;
+use OT\DataDictionary;
+
+/**
+ * 前台首页控制器
+ * 主要获取首页聚合数据
+ */
+class IndexController extends HomeController {
+
+	//系统首页
+    public function index(){
+               
+        $this->display();
+    }
+
+}
